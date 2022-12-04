7032761b4345ce380390d32a3a1b4cc3db743e86
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2022-12-04 01:32:39 -0400

    Modified logic for code reusability

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -1,20 +1,24 @@
 use std::fs;
 
+fn determine_priority(main_chunk: &str, remaining_chunks: Vec<&str>) -> u32 {
+    let common_item = main_chunk
+        .chars()
+        .find(|element| remaining_chunks.iter().all(|item| item.contains(*element)))
+        .unwrap();
+    let ascii_number = common_item as u32;
+    let priority = match common_item.is_ascii_lowercase() {
+        true => &ascii_number - 96,
+        false => &ascii_number - 38,
+    };
+    priority
+}
+
 fn process_part1(input_text: &String) -> u32 {
     input_text
         .lines()
         .map(|line| {
             let (first_half, second_half) = line.split_at(line.len() / 2);
-            let common_item = first_half
-                .chars()
-                .find(|element| second_half.contains(*element))
-                .unwrap();
-            let ascii_number = common_item as u32;
-            let priority = match common_item.is_ascii_lowercase() {
-                true => &ascii_number - 96,
-                false => &ascii_number - 38,
-            };
-            priority
+            determine_priority(first_half, vec![second_half])
         })
         .sum::<u32>()
 }
@@ -24,18 +28,7 @@ fn process_part2(input_text: &String) -> u32 {
         .lines()
         .collect::<Vec<&str>>()
         .chunks(3)
-        .map(|chunk| {
-            let common_item = chunk[0]
-                .chars()
-                .find(|element| chunk[1].contains(*element) & chunk[2].contains(*element))
-                .unwrap();
-            let ascii_number = common_item as u32;
-            let priority = match common_item.is_ascii_lowercase() {
-                true => &ascii_number - 96,
-                false => &ascii_number - 38,
-            };
-            priority
-        })
+        .map(|chunk| determine_priority(chunk[0], chunk[1..].to_vec()))
         .sum::<u32>()
 }
 
